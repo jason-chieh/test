bad04f3416b31018c4c8f4cc2d0981e842fc8a00
jason-g <a2012886@yahoo.com.tw>
2023-11-30 00:17:18 +0800

    test

---
+++ src/main/java/com/example/privateTest/PrivateTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.privateTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/privateTest/PrivateTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.privateTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
